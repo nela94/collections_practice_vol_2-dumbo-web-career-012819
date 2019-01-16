92b1e56a5a671af5d2495890e2ebd440199b4983
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-16 17:43:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
-# your code goes here
+def begins_with_r(tools)
+  tools.all? {|word| word.start_with?("r")}
+end
+def contain_a(array)
+  array.find_all {|word| word.include?("a")}
+end
+def first_wa(array)
+  array.find {|word| word[0] == "w" && word[1] == "a"}
+end
+def remove_non_strings(array)
+  array.delete_if { |obj| !(obj.is_a? String) }
+end
