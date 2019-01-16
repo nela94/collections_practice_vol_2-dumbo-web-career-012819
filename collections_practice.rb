d2dc568683c062ab7aefabe4bd3986b9797cecb2
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-16 18:17:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,3 +18,26 @@ def count_elements(array)
    end
    count.keys
  end
+ def merge_data(keys, data)
+  keys.each do |name_hash|
+    data.each do |hash|
+      name_hash.merge!(hash[name_hash[:first_name]])
+    end
+  end
+end
+def find_cool(array)
+  array.select {|entry| entry if entry.has_value?("cool")}
+end
+def organize_schools(schools)
+  by_location = {}
+  schools.each do |school, location_hash|
+    location_hash.each do |a, location|
+      if by_location[location] == nil
+        by_location[location] = [school]
+      else
+        by_location[location] << school
+      end
+    end
+  end
+  by_location
+end
